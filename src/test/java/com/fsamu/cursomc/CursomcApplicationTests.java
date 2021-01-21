2aca47b699492762ed2ba67d155225e35adb8d5b
Flavio Samu <fsamu40@gmail.com>
2021-01-21 13:23:56 -0300

    Projeto criado

---
+++ src/test/java/com/fsamu/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsamu.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
