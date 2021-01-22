77c407b01eb22d826c63e688ab90b5f62393373c
Flavio Samu <fsamu40@gmail.com>
2021-01-22 17:28:15 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/fsamu/cursomc/CursomcApplication.java
+++ src/main/java/com/fsamu/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.fsamu.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.fsamu.cursomc.domain.Cidade;
 import com.fsamu.cursomc.domain.Cliente;
 import com.fsamu.cursomc.domain.Endereco;
 import com.fsamu.cursomc.domain.Estado;
+import com.fsamu.cursomc.domain.Pagamento;
+import com.fsamu.cursomc.domain.PagamentoComBoleto;
+import com.fsamu.cursomc.domain.PagamentoComCartao;
+import com.fsamu.cursomc.domain.Pedido;
 import com.fsamu.cursomc.domain.Produto;
+import com.fsamu.cursomc.domain.enums.EstadoPagamento;
 import com.fsamu.cursomc.domain.enums.TipoCliente;
 import com.fsamu.cursomc.repositories.CategoriaRepository;
 import com.fsamu.cursomc.repositories.CidadeRepository;
 import com.fsamu.cursomc.repositories.ClienteRepository;
 import com.fsamu.cursomc.repositories.EnderecoRepository;
 import com.fsamu.cursomc.repositories.EstadoRepository;
+import com.fsamu.cursomc.repositories.PagamentoRepository;
+import com.fsamu.cursomc.repositories.PedidoRepository;
 import com.fsamu.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -92,6 +106,22 @@ public class CursomcApplication implements CommandLineRunner {
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
---
+++ src/main/java/com/fsamu/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,42 @@
+package com.fsamu.cursomc.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.fsamu.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Date dataPagamento;
+	private Date dataVencimento;
+
+	public PagamentoComBoleto() {
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido,
+			Date dataVencimento, Date dataPagamento) {
+		super(id, estado, pedido);
+		this.dataVencimento = dataVencimento;
+		this.dataPagamento = dataPagamento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+}
