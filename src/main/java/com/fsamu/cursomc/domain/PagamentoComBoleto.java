c373572035e9e257810f6d8fa610da51773148e4
Flavio Samu <fsamu40@gmail.com>
2021-01-23 06:27:48 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/fsamu/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/fsamu/cursomc/domain/PagamentoComBoleto.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fsamu.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -11,9 +13,12 @@ public class PagamentoComBoleto extends Pagamento {
 
 	private static final long serialVersionUID = 1L;
 	
-	private Date dataPagamento;
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimento;
 
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private Date dataPagamento;
+	
 	public PagamentoComBoleto() {
 	}
 
